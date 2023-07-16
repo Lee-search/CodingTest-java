6541d57542e80e9423e2cb8494c34a0681a4e4c6
Lee-search <ukyoung147@gmail.com>
2023-07-16 21:27:31 +0900

    디렉터리 생성..

---
+++ src/com/java/main/Soultion.java
@@ -0,0 +1,10 @@
+package com.java.main;
+
+import java.util.*;
+import java.io.*;
+
+public class Soultion {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
