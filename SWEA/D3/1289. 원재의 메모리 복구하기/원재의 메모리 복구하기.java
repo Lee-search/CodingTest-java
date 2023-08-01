2a8c07406cb8fbe6d21897a3e97d93685f109aeb
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-01 20:45:10 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 122 ms, Memory: 26,016 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Solution {
+     
+    public static String make(int[] p) {
+        String t = "";
+        for(int i = 0; i < p.length; i++) {
+            t += p[i];
+        }
+        return t;
+    }
+     
+    public static void push(int t, int s, int[] p) {
+        for(int i = s; i < p.length; i++) {
+            p[i] = t;
+        }
+    }
+     
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         
+        String n = br.readLine();
+        for(int test_case = 1; test_case <= Integer.parseInt(n); test_case++ ) {
+            String mem = br.readLine();
+            int[] origin = new int[mem.length()];
+            int[] local = new int[mem.length()];
+             
+            // origin 초기화
+            for(int i = 0; i < mem.length(); i++) {
+                origin[i] = mem.charAt(i) - '0';
+            }
+             
+            int count = 0;
+            // 같아질 때 까지 반복
+            while(!make(origin).equals(make(local))) {
+                // 첫 문자부터 비교
+                for(int i = 0; i < mem.length(); i++) {
+                    if(origin[i] != local[i]) {
+                        push(origin[i], i, local);
+                        count += 1;
+                        break;
+                    }
+                }
+            }
+            System.out.println("#" + test_case + " " + count);  
+        }
+    }
+}
