139a6d6ddc2d1f9811804feb97de8a656809ffe5
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-02 14:19:34 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1276 ms, Memory: 126072 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,7 +8,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		OutputStreamWriter osr = new OutputStreamWriter(System.out);
+//		OutputStreamWriter osr = new OutputStreamWriter(System.out);
+		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
@@ -35,8 +34,10 @@ public class Main {
 			for(int h = st_r; h <= end_r; h++) {
 				answer += plain[h][end_c] - plain[h][st_c - 1];
 			}
-			osr.write(answer + "\n");
+			sb.append(answer).append("\n");
+//			osr.write(answer + "\n");
 		}
-		osr.flush();
+		System.out.println(sb);
+//		osr.flush();
 	}
 }
