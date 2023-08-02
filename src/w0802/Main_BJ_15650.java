d97cd6db89a8b4e08d959ce9ff91254dcea30d5d
Lee-search <ukyoung147@gmail.com>
2023-08-02 15:46:06 +0900

    Solve_0802

---
+++ src/w0802/Main_BJ_15650.java
@@ -0,0 +1,36 @@
+package w0802;
+
+import java.util.Scanner;
+
+public class Main_BJ_15650 {
+	
+	public static int n, m;
+	public static int[] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[m];
+		
+		comb(0, 1);
+		sc.close();
+	}
+	
+	public static void comb(int count, int start) {
+		
+		if(count == m) {
+			for(int a : arr) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i = start; i <= n; i++) {
+			arr[count] = i;
+			comb(count + 1, i + 1);
+		}
+	}
+}
