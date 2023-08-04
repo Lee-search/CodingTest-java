b476ad83e80b7f21c3e9ecb911a01de93c7a2c5f
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-04 12:03:16 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 103 ms, Memory: 18,628 KB -BaekjoonHub

---
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		
+		//System.setIn(new FileInputStream(new File("./src/w0804/input_1.txt")));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for(int testCase = 1; testCase <= 10; testCase++) {
+			int len = Integer.parseInt(br.readLine());
+			Stack<Character> stack = new Stack<>();
+			int answer = 1;
+			
+			String line = br.readLine();
+			for(int i = 0; i < len; i++) {
+				char ch = line.charAt(i);
+				
+				if(ch == ')' || ch == ']' || ch == '}' || ch == '>') {
+					if(stack.isEmpty()) {
+						answer = 0;	// 괄호 합이 맞지 않음 -> 0
+						break;
+					}
+					
+					if(ch == ')' && stack.peek() == '(') stack.pop();
+					else if(ch == ']' && stack.peek() == '[') stack.pop();
+					else if(ch == '}' && stack.peek() == '{') stack.pop();
+					else if(ch == '>' && stack.peek() == '<') stack.pop();
+					else {
+						answer = 0;
+						break;
+					}
+				} else stack.push(ch);
+			}
+			
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+		} // end of tc
+		
+		System.out.println(sb);
+	} // end of main
+} // end of class
