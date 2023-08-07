54954b72066510563f91176dbf7fe495830fb5ff
Lee-search <ukyoung147@gmail.com>
2023-08-07 20:50:30 +0900

    보충_0807

---
+++ src/others/_pad/Main2.java
@@ -0,0 +1,99 @@
+package others._pad;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main2 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] plain = new int[19][19];
+        boolean[][] visited = new boolean[19][19];
+
+        // 입력 받기
+        for(int i = 0; i < 19; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < 19; j++) {
+                plain[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 각 칸에 대해서 5개가 완성되어있는지 검증
+        for(int r = 0; r < 19; r++) {
+            for(int c = 0; c < 19; c++) {
+                if(plain[r][c] == 1 || plain[r][c] == 2) {
+                    int d = plain[r][c];
+                    int[] sums = { 1, 1, 1, 1 };
+
+                    // 윗쪽, 왼쪽으론 계산할 필요 없음, 이미 훑고 왔음
+                    for(int i = 1; i < 6; i++) {
+                        // 아래
+                        if(r + 4 < 19) {
+                            if(plain[r + i][c] == plain[r + i - 1][c]) {
+                                sums[0] += 1;
+                            }
+                        }
+                        // 위
+                        if(r - 4 >= 0) {
+                            if(plain[r - i][c] == plain[r - i + 1][c]) {
+                                sums[0] += 1;
+                            }
+                        }
+
+                        // 오른쪽
+                        if(c + 4 < 19) {
+                            if(plain[r][c + i] == plain[r][c + i - 1]) {
+                                sums[1] += 1;
+                            }
+                        }
+                        // 왼쪽
+                        if(c - 4 >= 0) {
+                            if(plain[r][c - i] == plain[r][c - i + 1]) {
+                                sums[1] += 1;
+                            }
+                        }
+
+                        // 5시 대각선
+                        if(r + 4 < 19 && c + 4 < 19) {
+                            if(plain[r + i][c + i] == plain[r + i - 1][c + i - 1]) {
+                                sums[2] += 1;
+                            }
+                        }
+                        // 11시 대각선
+                        if(r - 4 >= 0 && c - 4 >= 0) {
+                            if(plain[r - i][c - i] == plain[r - i + 1][c - i + 1]) {
+                                sums[2] += 1;
+                            }
+                        }
+
+                        // 1시 대각선
+                        if(r - 4 >= 0 && c + 4 < 19) {
+                            if(plain[r - i][c + i] == plain[r - i + 1][c + i - 1]) {
+                                sums[3] += 1;
+                            }
+                        }
+                        // 7시 대각선
+                        if(r + 4 < 19 && c - 4 >= 0) {
+                            if(plain[r + i][c - i] == plain[r + i - 1][c - i + 1]) {
+                                sums[3] += 1;
+                            }
+                        }
+                    }
+
+                    // 하나라도 5개 있는지 확인
+                    if(sums[0] == 5 || sums[1] == 5 || sums[2] == 5 || sums[3] == 5) {
+                        System.out.println(Arrays.toString(sums));
+                        System.out.println(d);
+                        System.out.println((r + 1) + " " + (c + 1));
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(0);
+    }
+}
--- src/others/_pad/Solution.java
+++ src/others/_pad/Solution.java
@@ -1,15 +1,74 @@
 package others._pad;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * - 입력
+ * 금액정보 1차원 배열
+ * 이용계획 1차원 배열
+ * - 1월 ~ 12월 각 달마다 이용권 정하고, 금액 추가 작업
+ *      - 이용권 정해서 다음달로 전달
+ *      - 정한 이용권에 따른 금액도 다음달로 전달
+ * - 가저조건: 12월까지만 확인
+ * - 출력 포멧 맞춰서 출력
+ */
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
-        for (int test = 1; test <= T; test++) {
-            int answer = 0;
-            System.out.println("#" + test + " " + answer);
+    public static int[] tickets = new int[4];
+    public static int[] plans = new int[12];
+    public static int answer;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+
+        int T = stoi(br.readLine());
+        for(int testCase = 1; testCase <= T; testCase++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int i = 0; i < 4; i++)  // 이용권 가격
+                tickets[i] = stoi(st.nextToken());
+
+            answer = tickets[3];	// 최소값의 기준을 연권으로 잡고 시작
+
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < 12; i++) // 12개월 이용 계획
+                plans[i] = stoi(st.nextToken());
+
+            DFS(0, 0);
+            sb.append("#").append(testCase).append(" ").append(answer).append("\n");
         }
+
+        System.out.print(sb);
+    } // end of main
+
+    /**
+     * 
+     * @param month : 이용 개월 수
+     * @param sum : 이용 가격
+     */
+
+    public static void DFS(int month, int sum) {	// cnt: 0~11 + 1월
+
+        if(month >= 12) {
+            answer = Math.min(answer, sum);
+            return;
+        }
+
+        // 1일권
+        // 해당 월의 가격 저장
+        DFS(month + 1, sum + (plans[month] * tickets[0]));
+
+        // 1달권
+        DFS(month + 1, sum + tickets[1]);
+
+        // 3달권
+        DFS(month + 3, sum + tickets[2]);
     }
+
+    public static int stoi(String s) {
+        return Integer.parseInt(s);
+    } // end of stoi
 }
