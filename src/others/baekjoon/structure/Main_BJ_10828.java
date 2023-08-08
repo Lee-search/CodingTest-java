086ae26acaa9ba219be4cd95a0dc873dc89a1afb
Lee-search <ukyoung147@gmail.com>
2023-08-09 00:12:17 +0900

    Solve_0808

---
+++ src/others/baekjoon/structure/Main_BJ_10828.java
@@ -0,0 +1,71 @@
+package others.baekjoon.structure;
+
+/**
+ * 스택 : https://www.acmicpc.net/problem/10828
+ * 스택의 기본 개념 및 리스트 구현 이해 확인
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_10828 {
+
+    public static int N, size;
+    public static int[] stack;
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        stack = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            switch (st.nextToken()) {
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+                case "top":
+                    sb.append(top()).append("\n");
+                    break;
+            }
+        } // end of for
+
+        System.out.println(sb);
+    } // end of main
+
+    public static void push(int num) {
+        stack[size++] = num;
+    }
+
+    public static int pop() {
+        if(size == 0) return -1;
+        return stack[size-- - 1];
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if(size == 0) return 1;
+        return 0;
+    }
+
+    public static int top() {
+        if(size == 0)  return -1;
+        return stack[size - 1];
+    }
+} // end of class
---
+++ src/others/baekjoon/structure/Main_BJ_9012.java
@@ -0,0 +1,55 @@
+package others.baekjoon.structure;
+
+/**
+ * 괄호 : https://www.acmicpc.net/problem/9012
+ * 스택 자료구조 이해, 괄호가 열릴 때만 스택에 삽입하기
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main_BJ_9012 {
+
+    public static Stack<Character> stack;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int testCase = 0; testCase < N; testCase++) {    // 라인 입력
+
+            String line = br.readLine();
+
+            // 홀수개인 경우, 괄호가 닫힐 수 없음
+            if(line.length() % 2 != 0) {
+                sb.append("NO").append("\n");
+                continue;
+            }
+
+            stack = new Stack<>();
+            boolean flag = false;
+            for(int i = 0; i < line.length(); i++) {
+                char ch = line.charAt(i);
+
+                if (ch == '(') stack.push(ch);
+                else if (ch == ')') {
+
+                    if(!stack.isEmpty() /*&& stack.peek() == '('*/) stack.pop();
+                    else {
+                        flag = true;
+                        break;
+                    }
+                }
+            } // end of for
+
+            if(flag) sb.append("NO").append("\n");
+            else if(stack.isEmpty()) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
+        } // end of tc
+
+        System.out.println(sb);
+    } // end of main
+} // end of class
