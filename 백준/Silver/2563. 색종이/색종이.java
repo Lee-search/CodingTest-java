2b3bd6dbe133284cb693caac76d47361efa7355f
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-09 13:53:43 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int N, answer;
+	public static int[][] plain;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		N = stoi(br.readLine());
+		plain = new int[101][101];
+		
+		for(int testCase = 0; testCase < N; testCase++) {
+			st = new StringTokenizer(br.readLine());
+			int c = stoi(st.nextToken());
+			int r = stoi(st.nextToken());
+			
+			for(int i = 0; i < 10; i++) {
+				for(int j = 0; j < 10; j++) {
+					// 이미 한번 지나간 영역이면
+					if(plain[r + i][c + j] == 1) continue;
+					plain[r + i][c + j] = 1;
+					answer += 1;
+				}
+			}
+		} // end of tc
+		
+		System.out.println(answer);
+	} // end of main
+	
+	public static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
+}
