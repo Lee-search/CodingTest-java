7cdb2860a1d54fffb2b729206973668839161513
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-29 14:24:35 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12096 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,33 +1,46 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
+	
 	static int N;
-	static int[][] maps;
-
+	static int[] cost;
+	static int[] dp;
+	
 	public static void main(String[] args) throws Exception {
-		N = Integer.parseInt(br.readLine());
-		maps = new int[N][3];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < 3; i++) {
-			maps[0][i] = Integer.parseInt(st.nextToken());
-		}
-
-		int temp;
-		for (int i = 1; i < N; i++) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		N = stoi(br.readLine());
+		cost = new int[3 * N];
+		dp = new int[3 * N];
+		
+		for(int i= 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			temp = Integer.parseInt(st.nextToken());
-			maps[i][0] = Math.min(temp + maps[i - 1][1], temp + maps[i - 1][2]);
-			temp = Integer.parseInt(st.nextToken());
-			maps[i][1] = Math.min(temp + maps[i - 1][0], temp + maps[i - 1][2]);
-			temp = Integer.parseInt(st.nextToken());
-			maps[i][2] = Math.min(temp + maps[i - 1][0], temp + maps[i - 1][1]);
+			for(int j = 0; j < 3; j++)
+				cost[3 * i + j] = stoi(st.nextToken());
+		} // end of init
+		
+		dp[0] = cost[0];
+		dp[1] = cost[1];
+		dp[2] = cost[2];
+		
+		for(int i = 3; i < N * 3; i++) {
+			
+			if(i % 3 == 0) dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+			else if(i % 3 == 1) dp[i] = Math.min(dp[i - 2], dp[i - 4]) + cost[i];
+			else if(i % 3 == 2) dp[i] = Math.min(dp[i - 4], dp[i - 5]) + cost[i];
+			
+//			System.out.println(i + ", dp: " + Arrays.toString(dp));
 		}
-		System.out.println(Math.min(maps[N-1][0], Math.min(maps[N-1][1], maps[N-1][2])));
-	}
+		
+		System.out.println(Math.min(Math.min(dp[3 * N - 1], dp[3 * N - 2]), dp[3 * N - 3]));
+	} // end of main
+	
+	public static int stoi(String s) {
+		return Integer.parseInt(s);
+	} // end of stoi
 }
