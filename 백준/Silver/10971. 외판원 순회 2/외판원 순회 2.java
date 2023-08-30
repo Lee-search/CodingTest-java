8ea3aafad57e133062f13ee9d5861d73f4a4e3f2
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-30 13:28:21 +0900

    [Silver II] Title: 외판원 순회 2, Time: 360 ms, Memory: 13852 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,63 +1,79 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
-	static int N;
-	static int[][] maps;
-	static boolean[] visited;
-	static int minCost;
-	static int start;
-
-	public static void initial() throws Exception {
-		N = Integer.parseInt(br.readLine());
-		maps = new int[N][N];
-		visited = new boolean[N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				maps[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		minCost = Integer.MAX_VALUE;
-	}
-
-	public static void dfs(int depth, int now, int cost) {
-		int newCost;
-		if (depth == N - 1) {
-			newCost = maps[now][start];
-			if (newCost == 0)
-				return;
-			minCost = Math.min(minCost, cost + newCost);
-			return;
-		}
-
-		if (cost > minCost)
-			return;
-		
-		for (int i = 0; i < N; i++) {
-			newCost = maps[now][i];
-			if (newCost != 0 && !visited[i] && cost + newCost <= minCost) {
-				visited[i] = true;
-				dfs(depth + 1, i, cost + newCost);
-				visited[i] = false;
-			}
-		}
-
-	}
-
+	
+	static int N, answer;
+	static int[][] adjMatrix;
+	
+	static int[] selector;
+	static boolean[] isSelected;
+	
 	public static void main(String[] args) throws Exception {
-		initial();
-		for (int j = 0; j < N; j++) {
-			start = j;
-			visited[j] = true;
-			dfs(0, j, 0);
-			visited[j] = false;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		N = stoi(br.readLine());
+		answer = Integer.MAX_VALUE;
+		adjMatrix = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < N; j++) {
+				int n = stoi(st.nextToken());
+				if(n != 0) adjMatrix[i][j] = n;
+			}
+		} // end of init
+		
+		selector = new int[N];
+		isSelected = new boolean[N];
+		permutations(0);
+		
+		System.out.println(answer);
+	} // end of main
+	
+	public static void permutations(int cnt) {
+		
+		if(cnt == N) {
+			walk();
+			return;
+		} // basis
+		
+		for(int i = 0; i < N; i++) {
+			
+			if(isSelected[i]) continue;
+			
+			isSelected[i] = true;
+			selector[cnt] = i;
+			permutations(cnt + 1);
+			isSelected[i] = false;
 		}
-		System.out.println(minCost);
-	}
+	} // end of func
+	
+	public static void walk() {
+		
+		int total = 0;
+		
+		// 맨 마지막 위치에서 다시 처음으로 돌아와야함
+		if(adjMatrix[selector[N - 1]][selector[0]] == 0) return;
+		
+		for(int i = 1; i < N; i++) {
+			
+			int a = selector[i - 1];
+			int b = selector[i];
+			// 다음 칸으로 갈 수 없으면 종료
+			if(adjMatrix[a][b] == 0) return;
+			total += adjMatrix[a][b];
+		}
+		
+		total += adjMatrix[selector[N - 1]][selector[0]];
+		answer = Math.min(total, answer);
+	} // end of func
+	
+	public static int stoi(String s) {
+		return Integer.parseInt(s);
+	} // end of stoi
 }
