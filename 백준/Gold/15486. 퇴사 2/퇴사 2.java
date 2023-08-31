6908fcfd7c2455dea420d98deee954e4cc9ee579
Lee <33650294+Lee-search@users.noreply.github.com>
2023-08-31 21:23:20 +0900

    [Gold V] Title: 퇴사 2, Time: 656 ms, Memory: 302900 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N;
-    static int[][] cost;
     static int[] dp;
 
     public static void main(String[] args) throws IOException {
@@ -15,32 +14,20 @@ public class Main {
         StringTokenizer st = null;
 
         N = stoi(br.readLine());
-        cost = new int[N + 1][2];
+        dp = new int[N + 2];
+
         for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
+            // 해당 일에 드는 시간과 얻을 수 있는 비용
             int t = stoi(st.nextToken());
             int p = stoi(st.nextToken());
-            cost[i] = new int[] {t, p}; // 해당 일에 드는 시간과 얻을 수 있는 비용
+
+            // 입력과 동시에 dp 테이블에 삽입
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]); // 선택 X
+            if(i + t <= N + 1) {    // 선택
+                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
+            }
         } // end of init
-
-        dp = new int[N + 2];
-        for(int i = 1; i <= N; i++) {
-
-            int t = cost[i][0]; // 기간
-            int p = cost[i][1]; // 금액
-
-            // 선택하지 않음
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
-
-            // 선택함
-            if(i + t <= N) {
-                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
-            }
-            // 마지막날은 t가 1인 경우에만 일을 할 수 있음
-            else if(i + t == N + 1) {
-                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
-            }
-        }
 
         System.out.println(dp[N + 1]);
     } // end of main
