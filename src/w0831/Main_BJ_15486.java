739e2fd037b91df59b9ef368b4dbedd2e37447a7
Lee-search <ukyoung147@gmail.com>
2023-08-31 21:29:01 +0900

    SOLVED_0831

--- src/w0831/Main_BJ_15486.java
+++ src/w0831/Main_BJ_15486.java
@@ -1,45 +1,37 @@
 package w0831;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main_BJ_15486 {
 
     static int N;
-    static int[][] cost;
     static int[] dp;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
 
         N = stoi(br.readLine());
-        cost = new int[N + 1][2];
+        dp = new int[N + 2];
+
         for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
+            // 해당 일에 드는 시간과 얻을 수 있는 비용
             int t = stoi(st.nextToken());
             int p = stoi(st.nextToken());
-            cost[i] = new int[] {t, p}; // 해당 일에 드는 시간과 얻을 수 있는 비용
+
+            // 입력과 동시에 dp 테이블에 삽입
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]); // 선택 X
+            if(i + t <= N + 1) {    // 선택
+                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
+            }
         } // end of init
 
-        dp = new int[N + 1];
-        for(int i = 1; i <= N; i++) {
-            // 선택하지 않음
-            dp[i] = Math.max(dp[i], dp[i - 1]);
-
-            if(i + cost[i][0] <= N) { // 선택함
-                dp[i + cost[i][0]] = Math.max(dp[i + cost[i][0]], dp[i] + cost[i][1]);
-            }
-            // 마지막날은 cost[i][0]이 1인 경우에만 일을 할 수 있음
-            else if(i + cost[i][0] == N + 1) {
-                dp[i] = Math.max(dp[i], dp[i] + cost[i][1]);
-            }
-        }
-
-        System.out.println(dp[N]);
+        sb.append(dp[N + 1]);
+        System.out.println(sb);
     } // end of main
 
     public static int stoi(String s) {
