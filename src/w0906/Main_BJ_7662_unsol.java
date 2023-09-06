1a0b8ce7265b77c77cf1903da7633b2a738eb208
Lee-search <ukyoung147@gmail.com>
2023-09-06 19:31:56 +0900

    SOLVED_0906

--- src/w0906/Main_BJ_7662_unsol.java
+++ src/w0906/Main_BJ_7662_unsol.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main_BJ_7662 {
+public class Main_BJ_7662_unsol {
 
     static int K;
     static PriorityQueue<Integer> pqMax, pqMin;
