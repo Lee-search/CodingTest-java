3084e870e742e594e306cb1b76b93b7321c739cb
Lee <33650294+Lee-search@users.noreply.github.com>
2023-09-06 19:31:43 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 25292 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static int N;
+    static PriorityQueue<Integer> pq;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = stoi(br.readLine());
+        pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int n = stoi(br.readLine());
+            if (n == 0) {
+                if (pq.isEmpty()) sb.append("0\n");
+                else sb.append(pq.poll()).append("\n");
+            } else {
+                pq.offer(n);
+            }
+        }
+
+        System.out.println(sb);
+    } // end of main
+
+    public static int stoi(String s) {
+        return Integer.parseInt(s);
+    } // end of stoi
+}
