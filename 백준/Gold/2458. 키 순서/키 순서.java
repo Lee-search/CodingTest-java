4721473c99b03498e3dd6eb98ff03ee360aa1170
Lee <33650294+Lee-search@users.noreply.github.com>
2023-10-04 15:16:58 +0900

    [Gold IV] Title: 키 순서, Time: 788 ms, Memory: 41096 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,84 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M, answer, tmp;
+	static List<Integer>[] downList, upList;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        downList = new List[N + 1];
+        upList = new List[N + 1];
+
+        for(int i = 1; i <= N; i++) {
+            downList[i] = new ArrayList<>();
+            upList[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = stoi(st.nextToken());
+            int b = stoi(st.nextToken());
+            downList[a].add(b);
+            upList[b].add(a);
+        } // end of init 
+
+        for(int i = 1; i <= N; i++) {
+        	
+        	tmp = 0;
+        	visited = new boolean[N + 1];
+            down(i);	up(i);
+            // 노드의 위 아래 탐색 결과가 N - 1개가 나와야 순서 파악 가능
+            if(tmp == N - 1) answer += 1;
+        }
+		
+		System.out.print(answer);
+	} // end of main
+	
+	// 나보다 큰 사람 찾기
+	public static void down(int start) {
+		
+		visited[start] = true;
+		
+		for(int i = 0; i < downList[start].size(); i++) {
+			int n = downList[start].get(i);
+			if(!visited[n]) {
+				tmp += 1;
+				down(n);
+			}
+		}
+
+	} // end of down
+	
+	// 나보다 작은 사람 찾기
+	public static void up(int start) {
+		
+		visited[start] = true;
+		
+		for(int i = 0; i < upList[start].size(); i++) {
+			int n = upList[start].get(i);
+			if(!visited[n]) {
+				tmp += 1;
+				up(n);
+			}
+		}
+	} // end of up
+	
+	public static int stoi(String s) {
+		return Integer.parseInt(s);
+	} // end of stoi
+}
