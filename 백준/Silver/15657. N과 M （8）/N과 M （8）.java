c09cfc341d1649feccf3a5deed44896d8b2260c3
Lee <33650294+Lee-search@users.noreply.github.com>
2023-11-06 20:49:39 +0900

    [Silver III] Title: N과 M (8), Time: 120 ms, Memory: 16840 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+
+    static int N, M;
+    static int[] numbers, selector;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = parseInt(st.nextToken());
+        M = parseInt(st.nextToken());
+        numbers = new int[N];
+        selector = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = parseInt(st.nextToken());
+        } // end of init
+
+        Arrays.sort(numbers);
+        permutations(0, 0);
+        System.out.print(sb.toString());
+    } // end of main
+
+    public static void permutations(int start, int cnt) {
+
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selector[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        } // basis
+
+        for (int i = start; i < N; i++) {
+            selector[cnt] = numbers[i];
+            permutations(i, cnt + 1);
+        }
+    } // end of func
+}
