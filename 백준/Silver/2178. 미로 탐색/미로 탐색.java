ac9b42263e62bbb53f38d10f093b142311f19b39
Lee <33650294+Lee-search@users.noreply.github.com>
2024-10-13 03:14:59 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 13160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
-    static int[][] plain;
+    static int[][] plain, dist;
 
     public static void main(String[] args) throws IOException {
 
@@ -18,6 +19,10 @@ public class Main {
         N = stoi(st.nextToken());
         M = stoi(st.nextToken());
         plain = new int[N][M];
+        dist = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            Arrays.fill(dist[i], -1);
+        }
 
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
@@ -34,31 +39,26 @@ public class Main {
     static void BFS() {
 
         Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][M];
         
-        q.offer(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        q.offer(new int[]{0, 0});
+        dist[0][0] = 1;
 
         while (!q.isEmpty()) {
 
-            int r = q.peek()[0], c = q.peek()[1], cnt = q.poll()[2];
-            // 끝 지점에 도달, 항상 도달할 수 있는 경우만 주어지므로 추가 처리 X
-            if(r == N - 1 && c == M - 1) {
-                System.out.println(cnt);
-                return;
-            }
-
+            int r = q.peek()[0], c = q.poll()[1];
             for (int d = 0; d < 4; d++) {
 
                 int nr = r + dr[d];
                 int nc = c + dc[d];
                 if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                if(!visited[nr][nc] && plain[nr][nc] != 0) {
-                    q.offer(new int[]{nr, nc, cnt + 1});
-                    visited[nr][nc] = true;
-                }
+                if(dist[nr][nc] >= 0 || plain[nr][nc] == 0) continue;
+
+                dist[nr][nc] = dist[r][c] + 1;
+                q.offer(new int[]{nr, nc});
             }
         }
+
+        System.out.println(dist[N - 1][M - 1]);
     }
 
     static int stoi(String s) {
